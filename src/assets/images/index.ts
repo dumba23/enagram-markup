4d72530c106a51b85e3e1a3d757b16e02458b49e
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-04 14:55:34 +0400

    add necessarry images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,8 +2,22 @@ import Chrome from './icon-chrome.svg'
 import Profile from './icon-profile.svg'
 import Spellchecker from './icon-spellchecker.svg'
 import ArrowDown from './icon-arrow-down.svg'
+import Facebook from './icon-facebook.svg'
+import Moon from './icon-moon.svg'
+import Sun from './icon-sun.svg'
+import Question from './icon-question.svg'
+import Settings from './icon-settings.svg'
+import SpeechToText from './icon-speech-to-text.svg'
+import TextToSpeech from './icon-text-to-speech.svg'
 
 export const IconChrome = Chrome
 export const IconProfile = Profile
 export const IconSpellchecker = Spellchecker
 export const IconArrowDown = ArrowDown
+export const IconFacebook = Facebook
+export const IconSun = Sun
+export const IconMoon = Moon
+export const IconQuestion = Question
+export const IconSettings = Settings
+export const IconSpeechToText = SpeechToText
+export const IconTextToSpeech = TextToSpeech
