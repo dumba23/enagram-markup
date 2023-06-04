aa2f773c7b4d3a6c7e9c7c07ec5a02aa491c0b5e
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-04 12:12:11 +0400

    add header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { IconProfile, IconChrome, IconArrowDown } from '../../assets/images'
+import './style.css'
+
+const Header: React.FC = () => {
+  return (
+    <div className='header-container'>
+        <div className='header-content'>
+            <div className='container-margin-right center-content'>
+                <button className='header-button center-content chrome-button'>
+                    <img src={IconChrome} alt="chrome"/>
+                    <span>Add to Chrome</span>
+                </button>
+                <button className='header-button center-content upgrade-button'>
+                    Upgrade to Pro
+                </button>
+            </div>
+            <div className='center-content container-margin-right'>
+                <img src={IconProfile} alt="profile" className='header-profile-icon'/>
+                <img src={IconArrowDown} alt="dropdown" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
