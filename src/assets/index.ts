c157692b1f8403c69dc154468a3840a17f72f306
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-04 19:41:13 +0400

    add necessarry images and icons

---
+++ src/assets/index.ts
@@ -0,0 +1,53 @@
+import Chrome from './images/icon-chrome.svg'
+import Profile from './images/icon-profile.svg'
+import Spellchecker from './images/icon-spellchecker.svg'
+import ArrowDown from './images/icon-arrow-down.svg'
+import Facebook from './images/icon-facebook.svg'
+import Moon from './images/icon-moon.svg'
+import Sun from './images/icon-sun.svg'
+import Question from './images/icon-question.svg'
+import Settings from './images/icon-settings.svg'
+import SpeechToText from './images/icon-speech-to-text.svg'
+import TextToSpeech from './images/icon-text-to-speech.svg'
+import Bold from './images/icon-bold.svg'
+import Bullet from './images/icon-bullet.svg'
+import Check from './images/icon-check.svg'
+import Copy from './images/icon-copy.svg'
+import UK from './images/icon-uk.svg'
+import Germany from './images/icon-germany.png'
+import Georgia from './images/icon-georgia.png'
+import FormatClear from './images/icon-format-clear.svg'
+import H1 from './images/icon-h1.svg'
+import H2 from './images/icon-h2.svg'
+import Italic from './images/icon-italic.svg'
+import Link from './images/icon-link.svg'
+import Numeric from './images/icon-numeric.svg'
+import Underline from './images/icon-underline.svg'
+import Logo from './images/icon-logo.svg'
+
+export const IconChrome = Chrome
+export const IconProfile = Profile
+export const IconSpellchecker = Spellchecker
+export const IconArrowDown = ArrowDown
+export const IconFacebook = Facebook
+export const IconSun = Sun
+export const IconMoon = Moon
+export const IconQuestion = Question
+export const IconSettings = Settings
+export const IconSpeechToText = SpeechToText
+export const IconTextToSpeech = TextToSpeech
+export const IconBold = Bold
+export const IconBullet = Bullet
+export const IconCheck = Check
+export const IconCopy = Copy
+export const IconUK = UK
+export const IconGermany = Germany
+export const IconGeorgia = Georgia
+export const IconFormatClear = FormatClear
+export const IconH1 = H1
+export const IconH2 = H2
+export const IconItalic = Italic
+export const IconLink = Link
+export const IconNumeric = Numeric
+export const IconUnderline = Underline
+export const IconLogo = Logo
