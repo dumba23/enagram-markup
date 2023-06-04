05148fe8964d8502d079d1da501e8a078504c11c
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-04 12:11:38 +0400

    add necessarry icons

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import Chrome from './icon-chrome.svg'
+import Profile from './icon-profile.svg'
+import Spellchecker from './icon-spellchecker.svg'
+import ArrowDown from './icon-arrow-down.svg'
+
+export const IconChrome = Chrome
+export const IconProfile = Profile
+export const IconSpellchecker = Spellchecker
+export const IconArrowDown = ArrowDown
